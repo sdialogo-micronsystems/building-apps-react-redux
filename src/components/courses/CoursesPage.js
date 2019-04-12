5f376dda41ba24f722e755cdbdccfddd4d1323c5
sdialogo <sdialogo@micronsystems.com>
2019-04-12 13:52:49 +0800

    redux flow

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+class CoursesPage extends React.Component {
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => {
+      alert("Loading courses failed: " + error);
+    });
+  }
+  render() {
+    return (
+      <Fragment>
+        <h2>Courses</h2>
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </Fragment>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
